74f23e3d7924c9793a99b04363ef977d22ba162e
Hangwoo Cho <hangu4043@gmail.com>
2025-07-24 02:06:24 +0000

    [feat] add llm-as-judge-raw-sql evaluators class_20250724

--- evaluation/evaluators/llm-as-judge-raw-sql-evaluators.py
+++ evaluation/evaluators/llm-as-judge-raw-sql-evaluators.py
@@ -22,4 +22,10 @@ class LLMasJudgeRawSQL:
         self._flow = load_flow(source=prompty_path,model = {"configuration": model_config, "parameters": {}})
 
     
-    def __call__(self,)
+    def __call__(self,*, question: str, gold_sql:str, pred_sql: str, **kwargs):
+        llm_response = self._flow(question = question, gold_sql = gold_sql, pred_sql = pred_sql)
+        try:
+            response = json.load(llm_response)
+        except Exception as ex:
+            response = llm_response
+        return response
