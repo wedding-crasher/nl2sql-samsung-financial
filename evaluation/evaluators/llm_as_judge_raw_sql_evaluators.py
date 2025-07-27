f521d2870a7a741b04d05d07fc21e524800095fc
Hangwoo Cho <hangu4043@gmail.com>
2025-07-27 05:19:37 +0000

    [feat] add evaluators_test_code.ipynb_20250727

--- evaluation/evaluators/llm_as_judge_raw_sql_evaluators.py
+++ evaluation/evaluators/llm_as_judge_raw_sql_evaluators.py
@@ -2,22 +2,14 @@ import os
 import json
 import sys
 import pandas as pd
-from dotenv import load_dotenv
-from azure.ai.evaluation import AzureOpenAIModelConfiguration
 from promptflow.client import load_flow
 from tqdm import tqdm
 
-model_config = AzureOpenAIModelConfiguration(
-    azure_endpoint= os.environ["AZURE_ENDPOINT"],
-    azure_key = os.environ["AZURE_API_KEY"],
-    azure_deployment = os.environ["AZURE_4O_DEPLOYMENT"],
-    api_version = os.environ["AZURE_4O_API_VERSION"]
-)
+
 
 class LLMasJudgeRawSQL:
     def __init__(self, model_config):
-        current_dir = os.getcwd()
-        prompty_path = os.path.join(current_dir, "prompts/llm-as-judge-rawsql.prompty")
+        prompty_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "prompts/llm_as_judge_raw_sql.prompty"))
         # 1 Under bar represents don't directly access in the outside
         self._flow = load_flow(source=prompty_path,model = {"configuration": model_config, "parameters": {}})
 
